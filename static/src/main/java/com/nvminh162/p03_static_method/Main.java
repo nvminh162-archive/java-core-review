8dedc3a7bf0c44e3649304a2ba86097a038788c0
nvminh162 <nvminh162@gmail.com>
2026-06-15 14:40:48 +0700

    Add demo for static and non-static methods with `MathUtils` and `Main` classes

---
+++ static/src/main/java/com/nvminh162/p03_static_method/Main.java
@@ -0,0 +1,11 @@
+package com.nvminh162.p03_static_method;
+
+public class Main {
+    public static void main(String[] args) {
+        // static method: gọi trực tiếp qua tên class, không cần tạo object
+        System.out.println(MathUtils.square(5));
+
+        // non-static method: phải tạo object trước rồi mới gọi được
+        System.out.println(new MathUtils().max(10, 20));
+    }
+}
---
+++ static/src/main/java/com/nvminh162/p03_static_method/MathUtils.java
@@ -0,0 +1,11 @@
+package com.nvminh162.p03_static_method;
+
+public class MathUtils {
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public int max(int a, int b) {
+        return a > b ? a : b;
+    }
+}
