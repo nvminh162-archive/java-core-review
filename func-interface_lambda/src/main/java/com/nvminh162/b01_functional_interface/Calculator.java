998178f18a5236a9edb82ba1cb6ff31428354eb4
nvminh162 <nvminh162@gmail.com>
2026-06-24 13:34:01 +0700

    Add functional interface and lambda demo with default and static methods

--- func-interface_lambda/src/main/java/com/nvminh162/b01_functional_interface/Calculator.java
+++ func-interface_lambda/src/main/java/com/nvminh162/b01_functional_interface/Calculator.java
@@ -1,4 +1,24 @@
 package com.nvminh162.b01_functional_interface;
 
-public class Calculator {
+// Functional Interface là interface chỉ có 1 abstract method
+@FunctionalInterface
+public interface Calculator {
+
+    // Đây là abstract method duy nhất
+    // Lambda sẽ implement method này
+    int calculate(int a, int b);
+
+    // Functional Interface vẫn được phép có default method
+    default void printInfo() {
+        System.out.println("This is a calculator");
+    }
+
+    // Functional Interface vẫn được phép có static method
+    static void description() {
+        System.out.println("Calculator is used to calculate numbers");
+    }
+
+    // Nếu mở dòng dưới thì sẽ lỗi
+    // Vì Functional Interface chỉ được có 1 abstract method
+    // int subtract(int a, int b);
 }
--- func-interface_lambda/src/main/java/com/nvminh162/b01_functional_interface/Demo.java
+++ func-interface_lambda/src/main/java/com/nvminh162/b01_functional_interface/Demo.java
@@ -1,4 +1,21 @@
 package com.nvminh162.b01_functional_interface;
 
 public class Demo {
+    public static void main(String[] args) {
+
+        // Vì Calculator chỉ có 1 abstract method là calculate()
+        // nên Java cho phép dùng lambda để implement method đó
+
+        Calculator sum = (a, b) -> a + b;
+        Calculator sub = (a, b) -> a - b;
+
+        System.out.println(sum.calculate(10, 5)); // 15
+        System.out.println(sub.calculate(10, 5));
+
+        // Gọi default method
+        sum.printInfo();
+
+        // Gọi static method
+        Calculator.description();
+    }
 }
