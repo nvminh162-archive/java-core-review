2fb1b97db7c215be69e49ef1d2d105a047268157
nvminh162 <nvminh162@gmail.com>
2026-06-24 21:51:10 +0700

    Introduce generic class `Box<T>` with usage examples in `Demo` class

--- generic/src/main/java/com/nvminh162/b02_generic_class/Box.java
+++ generic/src/main/java/com/nvminh162/b02_generic_class/Box.java
@@ -1,4 +1,15 @@
 package com.nvminh162.b02_generic_class;
 
-public class Box {
+// T là type parameter
+// T có thể là String, Integer, User, Product...
+public class Box<T> {
+    private T value;
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
 }
--- generic/src/main/java/com/nvminh162/b02_generic_class/Demo.java
+++ generic/src/main/java/com/nvminh162/b02_generic_class/Demo.java
@@ -1,4 +1,32 @@
 package com.nvminh162.b02_generic_class;
 
 public class Demo {
+    public static void main(String[] args) {
+
+        // =====================================================
+        // Box<String>
+        // Lúc này T được hiểu là String
+        // =====================================================
+        Box<String> stringBox = new Box<>();
+        stringBox.setValue("Hello Generic");
+        String text = stringBox.getValue();
+        System.out.println(text);
+
+        // =====================================================
+        // Box<Integer>
+        // Lúc này T được hiểu là Integer
+        // =====================================================
+        Box<Integer> intBox = new Box<>();
+        intBox.setValue(100);
+        Integer number = intBox.getValue();
+        System.out.println(number);
+
+        // =====================================================
+        // Lợi ích:
+        // Cùng một class Box<T>
+        // nhưng dùng được cho nhiều kiểu dữ liệu khác nhau
+        // =====================================================
+        // stringBox.setValue(123); // lỗi vì stringBox chỉ nhận String
+        // intBox.setValue("Java"); // lỗi vì intBox chỉ nhận Integer
+    }
 }
