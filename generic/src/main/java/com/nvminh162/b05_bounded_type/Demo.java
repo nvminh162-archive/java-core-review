d18772879204471f68396fd0710ad3d4bb1e7da1
nvminh162 <nvminh162@gmail.com>
2026-06-24 22:31:32 +0700

    Add bounded type parameter example with `sum` method and demonstrations in `Demo` class

--- generic/src/main/java/com/nvminh162/b05_bounded_type/Demo.java
+++ generic/src/main/java/com/nvminh162/b05_bounded_type/Demo.java
@@ -1,4 +1,33 @@
 package com.nvminh162.b05_bounded_type;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Demo {
+
+    // =====================================================
+    // <T extends Number>
+    // Nghĩa là T chỉ được là Number hoặc class con của Number
+    // Ví dụ: Integer, Double, Long, Float...
+    // =====================================================
+    public static <T extends Number> double sum(List<T> numbers) {
+        double total = 0;
+        for (T number : numbers) {
+            // Vì T extends Number
+            // nên chắc chắn gọi được doubleValue()
+            total += number.doubleValue();
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> intNumbers = Arrays.asList(1, 2, 3);
+        List<Double> doubleNumbers = Arrays.asList(1.5, 2.5, 3.5);
+        System.out.println(sum(intNumbers));    // 6.0
+        System.out.println(sum(doubleNumbers)); // 7.5
+
+//         List<String> strings = Arrays.asList("A", "B");
+//         sum(strings); // lỗi vì String không extends Number
+    }
 }
