0a04789dd7cdd81ab122f0cefbb79f35305e17e8
nvminh162 <nvminh162@gmail.com>
2026-06-25 01:47:09 +0700

    Add `Runnable` and `DownloadTask` examples in `Demo` class to demonstrate multi-threading with threads, lambdas, and `Runnable` implementation

--- thread/src/main/java/com/nvminh162/b04_start_vs_run/Demo.java
+++ thread/src/main/java/com/nvminh162/b04_start_vs_run/Demo.java
@@ -1,4 +1,23 @@
 package com.nvminh162.b04_start_vs_run;
 
 public class Demo {
+    public static void main(String[] args) {
+
+        Runnable task = () -> { System.out.println("Running in thread: " + Thread.currentThread().getName()); };
+        Thread thread = new Thread(task);
+        // =====================================================
+        // run() chỉ là gọi method bình thường
+        // Không tạo thread mới
+        // Code vẫn chạy trong main thread
+        // =====================================================
+        System.out.println("Call run():");
+        thread.run();
+
+        // =====================================================
+        // start() mới tạo thread mới
+        // JVM sẽ tạo thread mới rồi gọi run() bên trong thread đó
+        // =====================================================
+        System.out.println("\nCall start():");
+        thread.start();
+    }
 }
