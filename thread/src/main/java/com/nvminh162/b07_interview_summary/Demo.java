b588f7468a23dac5018640b0214e6e72784e2e82
nvminh162 <nvminh162@gmail.com>
2026-06-25 11:48:33 +0700

    Remove `Main` class and add thread state lifecycle demonstration to `Demo` class

--- thread/src/main/java/com/nvminh162/b07_interview_summary/Demo.java
+++ thread/src/main/java/com/nvminh162/b07_interview_summary/Demo.java
@@ -1,4 +1,17 @@
 package com.nvminh162.b07_interview_summary;
 
 public class Demo {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread(() -> { System.out.println("Task running in: " + Thread.currentThread().getName()); });
+        thread.setName("Worker-Thread");
+
+        System.out.println("Before start: " + thread.getState());
+        thread.start();
+        System.out.println("After start: " + thread.getState());
+
+        thread.join();
+        System.out.println("After join: " + thread.getState());
+        System.out.println("Main finished");
+    }
 }
